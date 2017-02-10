8405776ca1710b15c9e9864ed8d2b1ff032b8d11
Mod-masta <jg5112@comcast.net>
2017-02-10 18:31:19 -0500

    Database_stuff

---
+++ app/src/main/java/muhlenberg/edu/cue/Database.java
@@ -0,0 +1,51 @@
+package muhlenberg.edu.cue;
+
+/**
+ * Created by jason on 2/7/17.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.*;
+
+import muhlenberg.edu.cue.util.text.FeedReaderContract;
+import muhlenberg.edu.cue.util.text.FeedReaderDbHelper;
+
+import static muhlenberg.edu.cue.ARSimpleApplication.getContext;
+
+public class Database {
+
+    public Database() {
+
+
+    }
+    public void POI() {
+
+        /**
+         *  Format:
+         *  insertPOI(name, short desc, long desc, latitude, longitude, activation range);
+         */
+
+        insertPOI("test", "test", "test", 1, 1, 1);
+
+    }
+
+        public long insertPOI(String name, String shortDesc, String longDesc, float latitude, float longitude, float activationRange) {
+            FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getContext());
+            SQLiteDatabase sql = mDbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_NAME, name);
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SHORT_DESC, shortDesc);
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_LONG_DESC, longDesc);
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_LATITUDE, latitude);
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_LONGITUDE, longitude);
+            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVATION_RANGE, activationRange);
+
+            //Inserts the POI and returns the ID
+            long newRowId = sql.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values);
+
+            return newRowId;
+        }
+
+}
---
+++ app/src/main/java/muhlenberg/edu/cue/util/text/FeedReaderDbHelper.java
@@ -0,0 +1,49 @@
+package muhlenberg.edu.cue.util.text;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static android.provider.BaseColumns._ID;
+import static muhlenberg.edu.cue.util.text.FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVATION_RANGE;
+import static muhlenberg.edu.cue.util.text.FeedReaderContract.FeedEntry.COLUMN_NAME_LATITUDE;
+import static muhlenberg.edu.cue.util.text.FeedReaderContract.FeedEntry.COLUMN_NAME_LONGITUDE;
+import static muhlenberg.edu.cue.util.text.FeedReaderContract.FeedEntry.COLUMN_NAME_LONG_DESC;
+import static muhlenberg.edu.cue.util.text.FeedReaderContract.FeedEntry.COLUMN_NAME_NAME;
+import static muhlenberg.edu.cue.util.text.FeedReaderContract.FeedEntry.COLUMN_NAME_SHORT_DESC;
+import static muhlenberg.edu.cue.util.text.FeedReaderContract.FeedEntry.TABLE_NAME;
+
+public class FeedReaderDbHelper extends SQLiteOpenHelper {
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_NAME_NAME + " TEXT," +
+                    COLUMN_NAME_SHORT_DESC + " TEXT," +
+                    COLUMN_NAME_LONG_DESC + " TEXT," +
+                    COLUMN_NAME_LATITUDE + " NUM," +
+                    COLUMN_NAME_LONGITUDE + " NUM)" +
+                    COLUMN_NAME_ACTIVATION_RANGE + " NUM," ;
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FeedReader.db";
+
+    public FeedReaderDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
