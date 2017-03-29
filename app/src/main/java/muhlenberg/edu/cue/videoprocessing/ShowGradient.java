654cdfbdac59d00409f2cc8b352a262f9a60d1f8
jmankhan <jmankhan1@gmail.com>
2017-03-29 12:26:12 -0400

    mostly integrated boofcv and beyondar. need to handle onpause and render gradient properly

---
+++ app/src/main/java/muhlenberg/edu/cue/videoprocessing/ShowGradient.java
@@ -0,0 +1,52 @@
+package muhlenberg.edu.cue.videoprocessing;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import boofcv.abst.filter.derivative.ImageGradient;
+import boofcv.android.VisualizeImageData;
+import boofcv.android.gui.VideoImageProcessing;
+import boofcv.factory.filter.derivative.FactoryDerivative;
+import boofcv.struct.image.GrayS16;
+import boofcv.struct.image.GrayU8;
+import boofcv.struct.image.ImageType;
+
+/**
+ * Created by Jalal on 3/22/2017.
+ */
+
+public class ShowGradient extends VideoImageProcessing<GrayU8> {
+    // Storage for the gradient
+    private GrayS16 derivX = new GrayS16(1, 1);
+    private GrayS16 derivY = new GrayS16(1, 1);
+
+    // computes the image gradient
+    private ImageGradient<GrayU8, GrayS16> gradient = FactoryDerivative.three(GrayU8.class, GrayS16.class);
+
+    public ShowGradient() {
+        super(ImageType.single(GrayU8.class));
+    }
+    /**
+     * Constructor
+     *
+     * @param imageType Type of image the video stream is to be converted to
+     */
+    public ShowGradient(ImageType<GrayU8> imageType) {
+        super(ImageType.single(GrayU8.class));
+    }
+
+    @Override
+    protected void declareImages(int width, int height) {
+        super.declareImages(width, height);
+
+        derivX.reshape(width, height);
+        derivY.reshape(width, height);
+    }
+
+    @Override
+    protected void process(GrayU8 image, Bitmap output, byte[] storage) {
+        gradient.process(image, derivX, derivY);
+        VisualizeImageData.colorizeGradient(derivX, derivY, -1, output, storage);
+
+    }
+}
